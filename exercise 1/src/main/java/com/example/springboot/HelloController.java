4e05443eb6d90bdf377ba301d602c2038a81647f
rickcicc <127338852+rickcicc@users.noreply.github.com>
2023-06-30 13:59:19 +0200

    esercizio fatto

--- exercise 1/src/main/java/com/example/springboot/HelloController.java
+++ exercise 1/src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,42 @@
 package com.example.springboot;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.StreamingHttpOutputMessage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Hello Word";
 	}
 
-}
+	@GetMapping("/greeting")
+	public ResponseEntity<String> greeting() {
+		String message = "Good Afternoon!";
+		return ResponseEntity.status(HttpStatus.OK).body(message);
+	}
+	@GetMapping("/info")
+	public ResponseEntity<String>info(){
+		return ResponseEntity.ok("information!!");
+	}
+	@GetMapping("/random")
+	public ResponseEntity<String>bool(){
+		boolean randoBoolean=new Random().nextBoolean();
+		if (randoBoolean){
+			return ResponseEntity.ok("true");
+		}else {
+			return ResponseEntity.badRequest().body("false");
+			}
+		}
+
+	}
+
+
+
